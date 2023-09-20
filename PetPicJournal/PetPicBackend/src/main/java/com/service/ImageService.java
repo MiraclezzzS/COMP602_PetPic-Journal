4a6cadcae85395b6b587855b35c0c5ef677fad0d
Liiiingb <ptq0754@autuni.ac.nz>
2023-09-20 21:06:31 +1200

    ImageService

---
+++ PetPicJournal/PetPicBackend/src/main/java/com/service/ImageService.java
@@ -0,0 +1,28 @@
+package com.service;
+
+import com.baomidou.mybatisplus.mapper.Wrapper;
+import com.baomidou.mybatisplus.service.IService;
+import com.utils.PageUtils;
+import com.entity.ImageClassEntity;
+import java.util.List;
+import java.util.Map;
+import com.entity.vo.ImageClassVO;
+import org.apache.ibatis.annotations.Param;
+import com.entity.view.ImageClassView;
+
+public interface ImageClassService extends IService<ImageClassEntity> {
+
+    PageUtils queryPage(Map<String, Object> params);
+    
+   	List<ImageClassVO> selectListVO(Wrapper<ImageClassEntity> wrapper);
+   	
+   	ImageClassVO selectVO(@Param("ew") Wrapper<ImageClassEntity> wrapper);
+   	
+   	List<ImageClassView> selectListView(Wrapper<ImageClassEntity> wrapper);
+   	
+   	ImageClassView selectView(@Param("ew") Wrapper<ImageClassEntity> wrapper);
+   	
+   	PageUtils queryPage(Map<String, Object> params,Wrapper<ImageClassEntity> wrapper);
+   	
+}
+
