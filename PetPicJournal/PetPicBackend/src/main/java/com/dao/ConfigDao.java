e2875a9ff28b1e5af5806fcf5247e16a761ed5ab
michaelpiaoAUT <hcb1764@autuni.ac.nz>
2023-10-19 08:28:36 +1300

    Changes needed to uploaded to main branch

--- PetPicJournal/PetPicBackend/src/main/java/com/dao/ConfigDao.java
+++
@@ -1,9 +0,0 @@
-
-package com.dao;
-
-import com.baomidou.mybatisplus.mapper.BaseMapper;
-import com.entity.ConfigEntity;
-
-public interface ConfigDao extends BaseMapper<ConfigEntity> {
-	
-}
--- PetPicJournal/PetPicFrontend/api/base.js
+++
@@ -1,5 +0,0 @@
-const base = {
-    url : "http://localhost:8080/ssm85bqd1pj/"
-	//url: "http://localhost:8081/spring/"
-}
-export default base
