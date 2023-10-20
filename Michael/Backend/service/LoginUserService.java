f644dec842f864c3d3dbc3920fce6131d7c82261
michaelpiaoAUT <hcb1764@autuni.ac.nz>
2023-10-20 17:57:38 +1300

    Backup file

---
+++ Michael/Backend/service/LoginUserService.java
@@ -0,0 +1,37 @@
+package com.service;
+
+import com.baomidou.mybatisplus.mapper.Wrapper;
+import com.baomidou.mybatisplus.service.IService;
+import com.utils.PageUtils;
+import com.entity.LoginUserEntity;
+import java.util.List;
+import java.util.Map;
+import com.entity.vo.LoginUserVO;
+import org.apache.ibatis.annotations.Param;
+import com.entity.view.LoginUserView;
+
+
+/**
+ * User
+ *
+ * @author 
+ * @email 
+ * @date 2023-08-26 17:38:30
+ */
+public interface LoginUserService extends IService<LoginUserEntity> {
+
+    PageUtils queryPage(Map<String, Object> params);
+    
+   	List<LoginUserVO> selectListVO(Wrapper<LoginUserEntity> wrapper);
+   	
+   	LoginUserVO selectVO(@Param("ew") Wrapper<LoginUserEntity> wrapper);
+   	
+   	List<LoginUserView> selectListView(Wrapper<LoginUserEntity> wrapper);
+   	
+   	LoginUserView selectView(@Param("ew") Wrapper<LoginUserEntity> wrapper);
+   	
+   	PageUtils queryPage(Map<String, Object> params,Wrapper<LoginUserEntity> wrapper);
+   	
+
+}
+
